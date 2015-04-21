706c41bf438b33eff02153f03f0d93c01cd1fa65
Yogi Joshi <joshi.yogi.ramdas@gmail.com>
2015-04-21 15:26:44 -0400

    Update JSPredicateEvaluator.java

--- src/main/java/rithm/defaultcore/JSPredicateEvaluator.java
+++ src/main/java/rithm/defaultcore/JSPredicateEvaluator.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 import rithm.core.PredicateState;
 import rithm.core.RiTHMPredicate;
-import jdk.nashorn.api.scripting.*;
+
 public class JSPredicateEvaluator extends DefaultPredicateEvaluator{
 	protected ArrayList<RiTHMPredicate> predList;
 	protected ScriptEngineManager mgr;
@@ -67,34 +67,19 @@ public class JSPredicateEvaluator extends DefaultPredicateEvaluator{
 			String Val = pState.getValue(key);
 			currBindings.put(key, Val);
 		}
-//		for(RiTHMPredicate pred: predList)
-//		{
-//			currBindings.put(pred.getTextDescription() + "Val","");
-//		}
 		try
 		{
-			ScriptObjectMirror obj = (ScriptObjectMirror)cscript.eval(currBindings);
-			for(String key1: obj.getOwnKeys(true))
-			{
-				System.out.println(obj.getMember(key1));
-			}
-			for(RiTHMPredicate pred: predList)
-			{
-				System.out.println("\"" + pred.getTextDescription() + "Val" + "\"");
-//				cscript.getEngine().get("\"" + pred.getTextDescription() + "Val" + "\"");
-//				Object obj = inv.invokeFunction("eval" + pred.getTextDescription());
-
-	//			prState.setValue(pred.getTextDescription(),Boolean.parseBoolean(obj.toString()));
-	//			String funcName = "eval" + pred.getTextDescription();
-	//			invocable.invoke()
-	//			prState.setValue( pred.getTextDescription(), Value);
-			}
+			Bindings obj = (Bindings)cscript.eval(currBindings);
+			for(String key1: obj.keySet())
+				this.prState.setValue(key1, Boolean.valueOf(obj.get(key1).toString()));
+			System.out.println("Hi" + this.prState);
 		}
 		catch(Exception se)
 		{
 			System.out.println(se.getMessage());
 		}
-//		prState.setTimestamp(Double.parseDouble(pState.getValue("timestamp")));
+		if(pState.getValue("timestamp") != null)
+			prState.setTimestamp(Double.parseDouble(pState.getValue("timestamp")));
 		return prState;
 	}
 	
